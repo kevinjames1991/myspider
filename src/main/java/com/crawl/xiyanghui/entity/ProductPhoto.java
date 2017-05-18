10fddbbf55aab3cbb723d319d590e76c177537bb
kevinjames <275648393@qq.com>
2017-05-18 17:53:47 +0800

    引入 lombok

--- src/main/java/com/crawl/xiyanghui/entity/ProductPhoto.java
+++ src/main/java/com/crawl/xiyanghui/entity/ProductPhoto.java
@@ -2,6 +2,11 @@ package com.crawl.xiyanghui.entity;
 
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ProductPhoto implements Serializable {
 
 	private static final long serialVersionUID = -8945159108095578798L;
@@ -12,46 +17,10 @@ public class ProductPhoto implements Serializable {
 	private String sizeId;		//尺码id
 	private String localUrl;	//本地图片url
 	
-	public String getLocalUrl() {
-		return localUrl;
-	}
-	public void setLocalUrl(String localUrl) {
-		this.localUrl = localUrl;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getSkuID() {
-		return skuID;
-	}
-	public void setSkuID(String skuID) {
-		this.skuID = skuID;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getColorId() {
-		return colorId;
-	}
-	public void setColorId(String colorId) {
-		this.colorId = colorId;
-	}
-	public String getSizeId() {
-		return sizeId;
-	}
-	public void setSizeId(String sizeId) {
-		this.sizeId = sizeId;
-	}
 	@Override
 	public String toString() {
-		return "DProductPhotoShop [id=" + id + ", skuID=" + skuID + ", url=" + url + ", colorId=" + colorId
-				+ ", sizeId=" + sizeId + ", localUrl=" + localUrl + "]";
+		return "ProductPhoto [id=" + id + ", skuID=" + skuID + ", url=" + url + ", colorId=" + colorId + ", sizeId="
+				+ sizeId + ", localUrl=" + localUrl + "]";
 	}
 	
 }
